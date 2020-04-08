e4a3a8044e71256e0d34e8c65c3e2d58c83be031
andrewhansen89 <andsen942@gmail.com>
2020-04-07 20:26:38 -0700

    Initial Commit
    Includes everything I've been working on to date.

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('customer/<str:pk>/', views.customer),
+    path('products/', views.products),
+]
